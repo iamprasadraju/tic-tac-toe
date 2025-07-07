8bf91e7c0788bc7f7ab3687bbf4cd36977f12c7e
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-07-08 00:45:56 +0530

    added winner eval

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import pygame
+import sys
 
 
 pygame.init()
@@ -11,11 +12,16 @@ HEIGHT = 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic Tac Toe")
 
-class Borad():
+class Board():
     def __init__(self):
         self.LINE_COLOR = (255, 255, 255)
         self.LINE_WIDTH = 2
         self.CELL_SIZE = WIDTH // 3
+
+        self.visited_cells = []
+        self.move_count  = 0
+        self.playerA_moves = [] # X moves
+        self.playerB_moves = [] # O moves
 
     def draw_board(self):
         screen.fill((0, 0, 0)) # Black
@@ -50,35 +56,55 @@ class Borad():
         pygame.draw.line(screen, self.LINE_COLOR, (end_x, start_y), (start_x, end_y), self.LINE_WIDTH)
 
 
-    def play(self):
+    def play(self, win_moves):
         self.draw_board()
         pygame.display.flip()
-        count  = 1
-        visited = []
+        
 
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    pygame.quit()
+                    sys.exit()
         
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    
+                elif event.type == pygame.MOUSEBUTTONDOWN and self.move_count < 9:  
                     mouseX, mouseY = pygame.mouse.get_pos()
                     clicked_row = mouseY // self.CELL_SIZE
                     clicked_col = mouseX // self.CELL_SIZE
-                    if (clicked_row, clicked_col) not in visited:
-                        if count % 2 == 0 and count < 9:
-                            self.draw_circle(clicked_row, clicked_col)
-                            visited.append((clicked_row, clicked_col))
-                            count += 1
-                            pygame.display.flip()
-                        else:
-                            self.draw_cross(clicked_row, clicked_col)
-                            visited.append((clicked_row, clicked_col))
-                            pygame.display.flip()
-                            count += 1
-        pygame.quit()
+                    move  = (clicked_row + 1, clicked_col + 1)
 
-game = Borad()
-game.play()
+                    if move not in self.visited_cells:
+                        if self.move_count % 2 == 0:
+                            self.draw_cross(clicked_row, clicked_col)
+                            self.playerA_moves.append(move)
+                        else:
+                            self.draw_circle(clicked_row, clicked_col)
+                            self.playerB_moves.append(move)
+                            
+                        self.visited_cells.append(move)
+                        self.move_count += 1
+                        pygame.display.flip()
+
+                        # Check for win after this move
+                        A = set(self.playerA_moves)
+                        B = set(self.playerB_moves)
+
+                        for line in win_moves:
+                            if set(line).issubset(A):
+                                print("Player A wins! - X")
+                                pygame.display.set_caption("Player A wins! - X")
+                                return self.playerA_moves, self.playerB_moves
+                            elif set(line).issubset(B):
+                                print("Player B wins! - O")
+                                pygame.display.set_caption("Player B wins! - O")
+                                return self.playerA_moves, self.playerB_moves
+
+                    else:
+                        print("Try Again !")
+
+            if self.move_count >= 9:
+                running = False
+
+        return self.playerA_moves, self.playerB_moves
+        
