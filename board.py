80c9200dc2780056ca50534b22d3df4a72e2c139
iamprasadraju <gls.prasadraju@gmail.com>
2026-04-05 14:29:49 +0530

    implementing custom board for tic tac toe

--- board.py
+++ board.py
@@ -1,84 +1,30 @@
-import math
+from typing import Tuple
 
 import pygame
 
-pygame.init()
 
-WIDTH = 500
-ROWS = 3
-CELL_SIZE = WIDTH // ROWS
-
-
-# Window size
-screen = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("Tic Tac Toe")
-running = True
-
-
-GRAY = (128, 128, 128)
-WHITE = (255, 255, 255)
-
-screen.fill(WHITE)
-
-
-X_IMAGE = pygame.transform.scale(pygame.image.load("assets/x.png"), (100, 100))
-O_IMAGE = pygame.transform.scale(pygame.image.load("assets/o.png"), (100, 100))
-
-
+# board ui and actions
 class Board:
-    def __init__(self):
-        self.running = True
+    def __init__(
+        self, screen, window_size: Tuple[int, int] = (500, 500)
+    ):  # window size default (500px * 500px)
+        self.width, self.height = window_size
 
-    # Apply the move on Screen
-    def apply_move(self, event, symbol):
-        m_x, m_y = event.pos  # Get mouse click co-ordinates
+        self.screen = screen
+        self.WHITE = (255, 255, 255)
+        self.BLACK = (0, 0, 0)
 
-        row = m_y // CELL_SIZE
-        col = m_x // CELL_SIZE
+    def draw_grid(self, grid_size: tuple):
+        rows, cols = grid_size
 
-        # center the image in the cell
-        pos_x = col * CELL_SIZE + (CELL_SIZE - 100) // 2
-        pos_y = row * CELL_SIZE + (CELL_SIZE - 100) // 2
+        # draw horizontal lines
+        for i in range(1, rows):
+            # Calculate y-coordinate for each horizontal divider
+            y_pos = (i * self.height) // rows
+            pygame.draw.line(self.screen, self.WHITE, (0, y_pos), (self.width, y_pos))
 
-        if symbol == "O":
-            screen.blit(O_IMAGE, (pos_x, pos_y))
-        elif symbol == "X":
-            screen.blit(X_IMAGE, (pos_x, pos_y))
-
-    def render_board(self):
-        pass
-
-    def draw_grid(self):
-        gap = WIDTH // ROWS
-
-        # Starting points
-        x = 0
-        y = 0
-
-        for i in range(ROWS):
-            x = i * gap
-
-            pygame.draw.line(screen, GRAY, (x, 0), (x, WIDTH), 3)
-            pygame.draw.line(screen, GRAY, (0, x), (WIDTH, x), 3)
-
-        pygame.display.flip()
-
-
-"""
-
-if __name__ == "__main__":
-    Game = Board()
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        Game.grid()
-        Game.apply_move()
-
-        pygame.display.flip()
-    pygame.quit()
-
-
-
-"""
+        # 2. Draw Vertical Lines
+        for j in range(1, cols):
+            # Calculate x-coordinate for each vertical divider
+            x_pos = (j * self.width) // cols
+            pygame.draw.line(self.screen, self.WHITE, (x_pos, 0), (x_pos, self.height))
