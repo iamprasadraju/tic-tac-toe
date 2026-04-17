0d4fdbdb0ef87f546491e3fda25d2a6cbbf8e6f9
iamprasadraju <gls.prasadraju@gmail.com>
2026-04-17 12:31:23 +0530

    #2 implemented handling for Human vs Human

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import random
 
 import pygame
@@ -7,10 +9,11 @@ from board import Board
 # Initialize the pygame display
 pygame.init()
 
-
 # pygame window size (in pixels)
 width = 600
 height = 600
+num_cells = (3, 3)
+
 
 # pygame.display.init()
 screen = pygame.display.set_mode(size=(width, height))
@@ -18,6 +21,13 @@ pygame.display.set_caption("Tic Tac Toe")
 clock = pygame.time.Clock()  # Limits FPS
 
 ttt_board = Board(screen)  # window size default (500px * 500px)
+
+
+def check_winner(player_moves):
+    for comb in ttt_board.all_win_moves:
+        if all(pos in player_moves for pos in comb):
+            return True
+    return False
 
 
 def run():
@@ -31,18 +41,52 @@ def run():
             elif ttt_board.board_state == "start":
                 if ttt_board.hvsh_btn.is_clicked(event):
                     ttt_board.board_state = "playing"
+                    pygame.display.set_caption(f"{ttt_board.current_player} Turn")
+
                 elif ttt_board.hvsai_btn.is_clicked(event):
                     ttt_board.board_state = "playing"
+                    pygame.display.set_caption(f"{ttt_board.current_player} Turn")
+            elif ttt_board.board_state == "game_over":
+                if ttt_board.play_again.is_clicked(event):
+                    ttt_board.reset_game()
 
             elif ttt_board.board_state == "playing":
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     row, col = ttt_board.get_mouse_gridnum(event.pos)
-                    player = random.choice(["X", "O"])
-                    ttt_board.player_move(player, (row, col))
+                    move = (row, col)
+                    max_moves = num_cells[0] * num_cells[1]
+
+                    # make move for current player
+                    if len(ttt_board.moves) < max_moves:
+                        if move not in ttt_board.moves:
+                            ttt_board.player_move((row, col))
+
+                            if ttt_board.current_player == "X":
+                                if check_winner(ttt_board.X_moves):
+                                    pygame.display.set_caption("X Wins!")
+                                    ttt_board.board_state = "game_over"
+                                    continue
+                                ttt_board.current_player = "O"
+                            else:
+                                if check_winner(ttt_board.O_moves):
+                                    pygame.display.set_caption("O Wins!")
+                                    ttt_board.board_state = "game_over"
+                                    continue
+                                ttt_board.current_player = "X"
+                                pygame.display.flip()
+
+                            pygame.display.set_caption(
+                                f"{ttt_board.current_player} Turn"
+                            )
+                        else:
+                            pygame.display.set_caption("Invalid move")
 
         if ttt_board.board_state == "start":
             ttt_board.screen.fill(ttt_board.BLACK)
             ttt_board.event_screen("home")
+
+        elif ttt_board.board_state == "game_over":
+            ttt_board.draw_win_screen()
 
         elif ttt_board.board_state == "playing":
             if not ttt_board.grid_drawn:
